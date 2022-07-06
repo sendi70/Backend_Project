7b9e7411999bb09311c1be3bc2db66e59b15d665
sendi70 <michal_bobula@wp.pl>
2022-07-06 22:22:33 +0200

    Add PasswordHasher

---
+++ AuthenticationServer/AuthenticationServer/Services/PasswordHasher.cs
@@ -0,0 +1,10 @@
+﻿namespace AuthenticationServer.Services
+{
+    public class PasswordHasher : IPasswordHasher
+    {
+        public string HashPassword(string password)
+        {
+            return BCrypt.Net.BCrypt.HashPassword(password);
+        }
+    }
+}
--- AuthenticationServer/AuthenticationServer/Startup.cs
+++ AuthenticationServer/AuthenticationServer/Startup.cs
@@ -1,3 +1,4 @@
+using AuthenticationServer.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -26,6 +27,7 @@ namespace AuthenticationServer
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddSingleton<IPasswordHasher, PasswordHasher>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.

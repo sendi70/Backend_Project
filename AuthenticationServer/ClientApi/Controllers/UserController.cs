024aa3c46d7a59688f979203a2a8a64b741605b6
sendi70 <michal_bobula@wp.pl>
2022-07-07 23:49:17 +0200

    Init Mikroservices 2 and add Register method

---
+++ AuthenticationServer/ClientApi/Controllers/UserController.cs
@@ -0,0 +1,54 @@
+﻿using ClientApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Net.Http.Json;
+
+namespace ClientApi.Controllers
+{
+    public class UserController : Controller
+    {
+        Uri baseAddress = new Uri("https://localhost:5001/api/Authentication");
+        HttpClient client;
+
+        public UserController()
+        {
+            client = new HttpClient();
+            client.BaseAddress = baseAddress;
+        }
+
+        public IActionResult Index()
+        {
+
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(User model)
+        {
+            //string data = JsonConvert.SerializeObject(model);
+            //var buffer= Encoding.UTF8.GetBytes(data);
+            //var byteContent = new ByteArrayContent(buffer);
+            //byteContent.Headers.ContentType = new MediaTypeHeaderValue("aplication/json");
+            //StringContent content = new StringContent(data, Encoding.UTF8, "aplication/json");
+            //HttpResponseMessage response = client.PostAsync(client.BaseAddress + "/register", byteContent).Result;
+            var user = new User { 
+                Email = model.Email,
+                Username = model.Username,
+                Password = model.Password,
+                ConfirmPassword = model.ConfirmPassword,
+            };
+            HttpResponseMessage response = client.PostAsJsonAsync(baseAddress + "/register", user).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+    }
+}
---
+++ AuthenticationServer/ClientApi/Models/User.cs
@@ -0,0 +1,10 @@
+﻿namespace ClientApi.Models
+{
+    public class User
+    {
+        public string Email { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
+    }
+}

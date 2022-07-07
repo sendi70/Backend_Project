17e878dff21c6120c68c3a68f5513450c666f85f
sendi70 <michal_bobula@wp.pl>
2022-07-07 18:59:23 +0200

    Implement database user repository

--- AuthenticationServer/AuthenticationServer/Program.cs
+++ AuthenticationServer/AuthenticationServer/Program.cs
@@ -1,5 +1,8 @@
+using AuthenticationServer.Models;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
@@ -13,7 +16,15 @@ namespace AuthenticationServer
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            IHost host = CreateHostBuilder(args).Build();
+            using(IServiceScope scope = host.Services.CreateScope())
+            {
+            using (AuthenticationDbContext context = scope.ServiceProvider.GetRequiredService<AuthenticationDbContext>())
+            {
+                context.Database.Migrate();
+            }
+            }
+                host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
---
+++ AuthenticationServer/AuthenticationServer/Services/DatabaseUserRepository.cs
@@ -0,0 +1,39 @@
+﻿using AuthenticationServer.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Threading.Tasks;
+
+namespace AuthenticationServer.Services
+{
+    public class DatabaseUserRepository : IUserRepository
+    {
+        private readonly AuthenticationDbContext _context;
+
+        public DatabaseUserRepository(AuthenticationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<User> Create(User user)
+        {
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+            return user;
+        }
+
+        public async Task<User> GetByEmail(string email)
+        {
+            return await _context.Users.FirstOrDefaultAsync(u => u.Email == email);
+        }
+
+        public async Task<User> GetById(Guid userId)
+        {
+            return await _context.Users.FindAsync(userId);
+        }
+
+        public Task<User> GetByUsername(string username)
+        {
+            return _context.Users.FirstOrDefaultAsync(u => u.Username == username);
+        }
+    }
+}

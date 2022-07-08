96766b75ad292701f7cda08a448c5ba6f6780bb9
sendi70 <michal_bobula@wp.pl>
2022-07-08 22:11:38 +0200

    Add migration

---
+++ AuthenticationServer/ClientApi/Migrations/20220708201043_Initial.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ClientApi.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "playgrounds",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Destination = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CordinatesX = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CordinatesY = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Capacity = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_playgrounds", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Age = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    City = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Events",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    StartTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    EndTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    OrganiserId = table.Column<int>(type: "int", nullable: false),
+                    PlaygroundId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Events", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Events_playgrounds_PlaygroundId",
+                        column: x => x.PlaygroundId,
+                        principalTable: "playgrounds",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Events_Users_OrganiserId",
+                        column: x => x.OrganiserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Event_Participans",
+                columns: table => new
+                {
+                    EventId = table.Column<int>(type: "int", nullable: false),
+                    UserId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Event_Participans", x => new { x.EventId, x.UserId });
+                    table.ForeignKey(
+                        name: "FK_Event_Participans_Events_EventId",
+                        column: x => x.EventId,
+                        principalTable: "Events",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Event_Participans_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Event_Participans_UserId",
+                table: "Event_Participans",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Events_OrganiserId",
+                table: "Events",
+                column: "OrganiserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Events_PlaygroundId",
+                table: "Events",
+                column: "PlaygroundId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Event_Participans");
+
+            migrationBuilder.DropTable(
+                name: "Events");
+
+            migrationBuilder.DropTable(
+                name: "playgrounds");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
--- AuthenticationServer/ClientApi/Models/Event.cs
+++ AuthenticationServer/ClientApi/Models/Event.cs
@@ -1,6 +1,23 @@
-﻿namespace ClientApi.Models
+﻿using BackEndProject.Models;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ClientApi.Models
 {
     public class Event
     {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public User Organiser { get; set; }
+
+
+        //Relationship
+        public List<Event_Participans> Event_Participans { get; set; }
+        public int PlaygroundId { get; set; }
+        [ForeignKey("PlaygroundId")]
+        public Playground Playground { get; set; }
     }
 }
